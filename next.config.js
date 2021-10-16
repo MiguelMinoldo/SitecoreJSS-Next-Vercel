baf86d20a69df947348c760a9c88a32b5c093862
VALTECH-COM\miguel.minoldo <miguel.minoldo@valtech.com>
2021-10-16 10:34:26 +0200

    Server url updated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const jssConfig = require('./src/temp/config');
 const packageConfig = require('./package.json').config;
 const { JSS_MODE_DISCONNECTED } = require('@sitecore-jss/sitecore-jss-nextjs');
 
-const disconnectedServerUrl = `http://localhost:${process.env.PROXY_PORT || 3042}/`;
+const disconnectedServerUrl = `http://a108-2a01-cb00-642-f100-f4f4-d758-4ae5-75df.ngrok.io:${process.env.PROXY_PORT || 3042}/`;
 const isDisconnected = process.env.JSS_MODE === JSS_MODE_DISCONNECTED;
 
 // A public URL (and uses below) is required for Sitecore Experience Editor support.
